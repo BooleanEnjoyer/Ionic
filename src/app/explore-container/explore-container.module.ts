9731733841d082bd9c2b76bfda0d64c162ce2df2
BooleanEnjoyer <259745@student.pwr.edu.pl>
2023-03-21 21:53:18 +0100

    Change 1

--- src/app/explore-container/explore-container.module.ts
+++ src/app/explore-container/explore-container.module.ts
@@ -5,11 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { ExploreContainerComponent } from './explore-container.component';
-import {HeroesPage} from "../heroes/heroes.component";
+
 
 @NgModule({
   imports: [ CommonModule, FormsModule, IonicModule],
-  declarations: [ExploreContainerComponent, HeroesPage],
+  declarations: [ExploreContainerComponent],
   exports: [ExploreContainerComponent]
 })
 export class ExploreContainerComponentModule {}
