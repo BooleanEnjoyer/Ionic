eab1110b1ee98ba6075a78663c365ffc22df22d0
BooleanEnjoyer <259745@student.pwr.edu.pl>
2023-03-21 22:59:44 +0100

    Change 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'detail/:id', component: HeroDetailComponent
   }
 ];
 @NgModule({
