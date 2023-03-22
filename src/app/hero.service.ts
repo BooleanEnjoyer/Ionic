684a051dcd140aebd72134f4849c1183421ed599
BooleanEnjoyer <259745@student.pwr.edu.pl>
2023-03-22 12:31:08 +0100

    Change 1

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -88,12 +88,18 @@ export class HeroService {
   }
 
   /** PUT: update the hero on the server */
-  updateHero(hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap(_hh => this.log(`updated hero id=${hero.id}`)),
-      catchError(this.handleError<any>('updateHero'))
+  updateHero(hero: Hero): Observable<Hero> {
+    return this.http.put<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((updateHero: Hero) => this.log(`updated hero id=${updateHero.id}`)),
+      catchError(this.handleError<Hero>('updateHero'))
     );
   }
+  // updateHero(hero: Hero): Observable<Hero> {
+  //   return this.http.put<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+  //     tap((updateHero: Hero) => this.log(`updated hero id=${updateHero.id}`)),
+  //     catchError(this.handleError<Hero>('updateHero'))
+  //   );
+  // }
 
   /**
    * Handle Http operation that failed.
